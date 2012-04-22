cb2b9a9bef5451c672133c7cb197eba1ba263f63
sauloo <saulinho.francisco@gmail.com>
2012-04-22 10:39:52 -0300

    Aumentei uma linha

--- src/br/edu/fasa/vendas/domainModel/Cliente.java
+++ src/br/edu/fasa/vendas/domainModel/Cliente.java
@@ -17,6 +17,7 @@ import javax.persistence.*;
 
 public class Cliente implements Serializable{
     
+    
     @Id
     @GeneratedValue(strategy= GenerationType.IDENTITY)
     @Column(name="codigo")
